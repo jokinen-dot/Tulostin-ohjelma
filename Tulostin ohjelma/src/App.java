25b7753338137969c9b3615cdd53e6d33c1133f5
jokinen-dot <vili.v.jokinen@gmail.com>
2026-09-15 14:00:00 +0300

    Create App.java
    Tulostin-ohjelman toteutus

---
+++ Tulostin ohjelma/src/App.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+    // Tulostin ohjelman muuttujat
+
+    String tekija = "Vili";
+    double luku1 = 5;
+    double luku2 = 2;
+    double tulo = luku1 * luku2;
+    double summa = luku1 + luku2;
+    double erotus = luku1 - luku2;
+    double jako = luku1 / luku2;
+
+
+
+    // Tulostin ohjleman tulostukset
+
+
+    System.out.println("Ohjelman tekija.");
+
+
+    System.out.println("Ohjelman tekija on: " + tekija);
+        
+
+        
+    System.out.println("Luku1-muuttujan arvo on " + luku1);
+        
+
+
+    System.out.println("Luku2-muuttujan arvo on " + luku2);
+
+
+    System.out.println("Lukujen tulo : " + tulo);
+
+
+    }
+}
